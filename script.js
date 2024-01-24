38e94ad6d53a69e88da7ab46b7c32e41f64c123a
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-01-24 16:42:28 -0500

    add function that obtains the player's choice
    the function obtains the player's choice and then
    checks with a dictionary object if it is correct

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 "use strict";
 
+const who_wins_who = {
+    'Rock': 'Scissors',
+    'Paper': 'Rock',
+    'Scissors': 'Rock',
+}
+
+function getPlayerChoice() {
+    while (true) {
+        let choice = prompt('choose rock paper or scissors:');
+        choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+        if (choice !== undefined) return choice;
+    }
+}
+
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random() * 3)];
