fd873f09c97030d3b4455e2915859221eeee5a46
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-01-26 20:35:31 -0500

    finish the application

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 const who_wins_who = {
     'Rock': 'Scissors',
     'Paper': 'Rock',
-    'Scissors': 'Rock',
+    'Scissors': 'Paper',
 }
 
 function getPlayerChoice() {
     while (true) {
         let choice = prompt('choose rock paper or scissors:');
         choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-        if (who_wins_who[choice] !== undefined) return choice;
+        if (choice in who_wins_who) return choice;
     }
 }
 
@@ -19,15 +19,31 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    while (playerSelection === computerSelection) {
+function playRound(playerChoice, computerChoice) {
+    while (playerChoice === computerChoice) {
         console.log('You tied! re-playing the round');
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
+        playerChoice = getPlayerChoice();
+        computerChoice = getComputerChoice();
     }
-    if (who_wins_who[playerSelection] === computerSelection) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-    }
+    if (who_wins_who[playerChoice] === computerChoice) {
+        return `You win! ${playerChoice} beats ${computerChoice}`;
+    } 
+    return `You lose! ${computerChoice} beats ${playerChoice}`;
 }
+
+function game() {
+    let player_wins = 0;
+    let computer_wins = 0;
+    for (let i = 0; i < 5; i++) {
+        const winner = playRound(getPlayerChoice(), getComputerChoice());
+        console.log(winner);
+        if (winner.includes('win')) player_wins ++;
+        else computer_wins ++;
+    }
+    if (player_wins > computer_wins) console.log(`The player has won the best of five games`);
+    else console.log(`The computer has won the best of five games\n`);
+    console.log(`Player wins: ${player_wins}\nComputer wins: ${computer_wins}`);
+}
+
+game();
+
