324b232b90d95bbe01e9e5ed8938ea77057aa6df
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-02-08 20:49:56 -0500

    remove logic that plays five times on console

--- script.js
+++ script.js
@@ -31,20 +31,4 @@ function playRound(playerChoice, computerChoice) {
     return `You lose! ${computerChoice} beats ${playerChoice}`;
 }
 
-function game() {
-    let player_wins = 0;
-    let computer_wins = 0;
-    for (let i = 0; i < 5; i++) {
-        const winner = playRound(getPlayerChoice(), getComputerChoice());
-        console.log(winner);
-        if (winner.includes('win')) player_wins ++;
-        else computer_wins ++;
-    }
-    if (player_wins > computer_wins) console.log(`The player has won the best of five games`);
-    else console.log(`The computer has won the best of five games\n`);
-    
-    console.log(`Player wins: ${player_wins}\nComputer wins: ${computer_wins}`);
-}
-
-game();
 
